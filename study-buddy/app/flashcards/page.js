b2bd88cd6779f4619fd8391946b83d282a47454d
sunday004 <agbidisunday@gmail.com>
2024-09-29 10:22:51 -0500

    render saved cards

--- study-buddy/app/flashcards/page.js
+++ study-buddy/app/flashcards/page.js
@@ -19,6 +19,7 @@ import {
     CardActionArea,
   } from '@mui/material'
 
+  
 export default function Flashcard() {
     const { isLoaded, isSignedIn, user } = useUser()
     const [flashcards, setFlashcards] = useState([])
